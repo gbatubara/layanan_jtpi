4ff37886081cb600930dc090989a74d713d6bd0d
rezaantika03 <38091165+rezaantika03@users.noreply.github.com>
2019-04-15 15:56:56 +0700

    updatehomepage

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,6 +5,73 @@
   <?php if (!$this->session->userdata('login')) : ?>
     <p><center>Silahkan login untuk detail lebih lanjut</p>
 <?php endif; ?>
+
+
+<!-- <div class="cycle-slideshow">
+    <span class="cycle-prev">&#9001;</span> <!-- Untuk membuat tanda panah di kiri slider -->
+    <!--<span class="cycle-next">&#9002;</span> <!-- Untuk membuat tanda panah di kanan slider -->
+    <!--<span class="cycle-pager" style="padding-bottom:30px;"></span>  <!-- Untuk membuat tanda bulat atau link pada slider -->
+    <!--<image src="http://localhost/layanan_jtpi-master/assets/image/slide1.jpg" alt="Gambar Pertama">
+    <image src="http://localhost/layanan_jtpi-master/assets/image/slide2.jpg" alt="Gambar Kedua">
+    <image src="http://localhost/layanan_jtpi-master/assets/image/slide3.jpg" alt="Gambar Ketiga">
+    <image src="http://localhost/layanan_jtpi-master/assets/image/slide4.jpg" alt="Gambar Keempat">  
+</div>-->
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="assets/image/slide1.jpg" alt="Gambar 1">
+      <div class="carousel-caption">
+        <h3>Jurusan Teknologi Produksi dan Industri</h3>
+        <p>Institut Teknologi Sumatera</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="assets/image/slide2.jpg" alt="Gambar 2">
+      <div class="carousel-caption">
+        <h3>Gerbang ITERA</h3>
+        <p>Institut Teknologi Sumatera</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="assets/image/slide3.jpg" alt="Gambar 3">
+      <div class="carousel-caption">
+        <h3>Gedung Kuliah Umum</h3>
+        <p>Institut Teknologi Sumatera</p>
+      </div>
+    </div>
+
+      <div class="item">
+      <img src="assets/image/slide4.jpg" alt="Gambar 4">
+      <div class="carousel-caption">
+        <h3>Asrama ITERA</h3>
+        <p>Institut Teknologi Sumatera</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
         </div>
       </div>
   </div>
