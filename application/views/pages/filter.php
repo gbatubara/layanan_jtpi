6a7c21ad40314c335e8b20631265f537e9aac2b2
ferdionaufal <naufalharyadi@gmail.com>
2019-04-22 16:09:02 +0700

    updatecssvalidasi

--- application/views/pages/filter.php
+++ application/views/pages/filter.php
@@ -1,85 +1,14 @@
 <!DOCTYPE html>
 <html><head>
 	<title>filter data di php codeigniter</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/bootstrap/css/tablefilter.css">
 </head>
 
-<style type="text/css">
-.table {
-  font-family: Arial, Helvetica, sans-serif;
-  color: #666;
-  text-shadow: 1px 1px 0px #fff;
-  background: #eaebec;
-  border: #ccc 1px solid;
-  margin-top: 10px;
-  margin-bottom: 20px;
-}
-
-.table th {
-  padding: 15px 35px;
-  border-left:1px solid #e0e0e0;
-  border-bottom: 1px solid #e0e0e0;
-  background: #ededed;
-}
-
-.table th:first-child{
-  border-left:none;
-}
-
-.table tr {
-  text-align: center;
-  padding-left: 20px;
-}
-
-.table td:first-child {
-  text-align: left;
-  padding-left: 20px;
-  border-left: 0;
-}
-
-.table td {
-  padding: 15px 35px;
-  border-top: 1px solid #ffffff;
-  border-bottom: 1px solid #e0e0e0;
-  border-left: 1px solid #e0e0e0;
-  background: #fafafa;
-  background: -webkit-gradient(linear, left top, left bottom, from(#fbfbfb), to(#fafafa));
-  background: -moz-linear-gradient(top, #fbfbfb, #fafafa);
-}
-
-.table tr:last-child td {
-  border-bottom: 0;
-}
-
-table tr:last-child td:first-child {
-  -moz-border-radius-bottomleft: 3px;
-  -webkit-border-bottom-left-radius: 3px;
-  border-bottom-left-radius: 3px;
-}
-
-table tr:last-child td:last-child {
-  -moz-border-radius-bottomright: 3px;
-  -webkit-border-bottom-right-radius: 3px;
-  border-bottom-right-radius: 3px;
-}
-
-table tr:hover td {
-  background: #f2f2f2;
-  background: -webkit-gradient(linear, left top, left bottom, from(#f2f2f2), to(#f0f0f0));
-  background: -moz-linear-gradient(top, #f2f2f2, #f0f0f0);
-}
-
-
-
-
-
-
-
-</style>
 
 <body>
-	<div class="container">
+	<div class="container" style="background:#fff;margin-top:0px; padding-top:30px; padding-bottom:15px; border-bottom:solid thin #e8e8e8; box-shadow:         0px -6px 22px 0px rgba(0, 0, 0, 0.2); border-radius: 3px;">
 		<div class="row">
-			<h1 align="center">Filter data di php codeigniter</h1>
+			<h1 align="center">Daftar Mahasiswa Pengajuan KP</h1>
 		<br>
 		<div class="col-md-3">
 
@@ -98,7 +27,7 @@ table tr:hover td {
 			<table class="table table-hover table-striped" id="mahasiswa">
 					<thead>
 						<tr>
-						<td>#</td>
+						<td>No</td>
 						<td>Nama</td>
 						<td>Nim</td>
 						<td>Email</td>
