a0122d618ab4ee7da8d3eb116138892638ed2d41
artwin22 <muhamad.14116050@student.itera.ac.id>
2019-05-05 17:42:51 +0700

    Update kalenderakademik.php

--- application/views/pages/kalenderakademik.php
+++ application/views/pages/kalenderakademik.php
@@ -3,8 +3,6 @@
           <div class="col-md-12">
 <p><h1><center>Kalender Akademik</p></h1>
 <center><img src="<?php echo base_url()?>assets/images/kalenderakademik.png" class="user-image" alt="User Image">
-  <?php if (!$this->session->userdata('login')) : ?>
-  <?php endif; ?>
         </div>
       </div>
   </div>
